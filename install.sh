996340b3564a0dfcd95d1a04849dad868a9d6ea9
Hashim <13991048+Prim69@users.noreply.github.com>
2022-08-02 15:00:01 -0400

    update go 1.19

--- install.sh
+++ install.sh
@@ -32,10 +32,10 @@ while getopts "mg" option; do
                 fi
             ;;
         g)
-              echo "Starting the installation of GoLang\n"
-              wget "https://go.dev/dl/go1.18.4.linux-amd64.tar.gz"
-              sudo tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
-              rm go1.18.4.linux-amd64.tar.gz
+              echo "Starting the installation of GoLang 1.19\n"
+              wget "https://go.dev/dl/go1.19.linux-amd64.tar.gz"
+              sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+              rm go1.19.linux-amd64.tar.gz
               echo "PATH=$PATH:/usr/local/go/bin" >> ~/.profile
               source ~/.profile
            ;;
