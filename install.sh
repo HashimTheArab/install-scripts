772957cce160baadd88e7cacc66c21c2adc4a804
Hashim <13991048+Prim69@users.noreply.github.com>
2022-09-10 13:16:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 for arg in "$@"; do
   shift
   case ${arg} in
-    "-mongo") set -- "$@" "-m" ;;
+    "-mongo6") set -- "$@" "-m" ;;
+    "-mongo4") set -- "$@" "-z" ;;
     "-go") set -- "$@" "-g" ;;
     *)        set -- "$@" "$arg"
   esac
@@ -10,7 +11,7 @@ done
 while getopts "mg" option; do
     case ${option} in
         m)
-                echo "Starting the installation of Mongo...\nAdding key\n\n"
+                echo "Starting the installation of Mongo 6.0...\nAdding key\n\n"
                 sleep 2
                 sudo apt-get install gnupg
                 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
@@ -18,6 +19,28 @@ while getopts "mg" option; do
                 sleep 2
                 touch /etc/apt/sources.list.d/mongodb-org-6.0.list
                 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+                sleep 2
+                echo "\n\nUpdating Packages...\n\n"
+                sleep 2
+                sudo apt-get update
+                echo "\n\nActually installing mongo!\n\n"
+                sleep 2
+                sudo apt-get install -y mongodb-org
+                sleep 2
+                read -p "Mongo installation finished - Do you want to start the Mongo service? (y/n): " answer
+                if [ "$answer" == "y" ]; then
+                    sudo systemctl start mongod
+                fi
+            ;;
+        z)
+                echo "Starting the installation of Mongo 4.0...\nAdding key\n\n"
+                sleep 2
+                sudo apt-get install gnupg
+                wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
+                echo "\n\nCreating list file...\n\n"
+                sleep 2
+                touch /etc/apt/sources.list.d/mongodb-org-4.0.list
+                echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
                 sleep 2
                 echo "\n\nUpdating Packages...\n\n"
                 sleep 2
